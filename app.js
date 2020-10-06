fe15fa88a3a0704d880fd8068bb14abd96b8f217
Scott Nelson <snelson.seattle@gmail.com>
2020-10-05 17:39:34 -0700

    Fully working application. Updated readme.md

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     // Capture UI Elements
     let currentDay = $("#currentDay");
+
+    // State Variables
     let currentHour = parseInt(moment().format('k'));
     
     setInterval(timePrint, 1000);  // Updates the date and time data every second
@@ -10,19 +12,20 @@ $(document).ready(function() {
     setInterval(function(){
         // This function is checking to see if the hour on the clock has changed. If it has, then it will call the updateUI function to 
         // change the color of the hourly events accordingly.
-
+        // ************ This is currently not working ****************
         let checkHour = parseInt(moment().format('k'));
         
         if(checkHour > currentHour){
-            updateUI();
             currentHour = checkHour;
+            updateUI();
+            
         }
-    }, 1000);
-   
-    
 
-    
+    }, 1000);    
+
+    // Event Handlers
     $(".saveBtn").on("click", saveEvent);
+
  });
 
 
@@ -32,9 +35,17 @@ function timePrint(){
 }
 
 function saveEvent(){
-    let storageKey = $(this).attr("data-time");    
+    let currentHour = parseInt(moment().format('k'));  // Locally scoped currentHour variable
     let event = $("#" + $(this).attr("data-time") + "-event");
-    localStorage.setItem(storageKey, event.val());
+    let storageKey = $(this).attr("data-time");
+
+    // Check to make sure the hour isn't past
+    if(parseInt(storageKey) >= currentHour) {        
+        localStorage.setItem(storageKey, event.val());    
+    }else {
+        alert("This hour has already passed!");
+        event.val("");
+    }    
     
 
 }
